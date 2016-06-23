e3544d7769fe981b9142d45c03d937a24687dcd2
Vijay Pillai <vpillai13@simons-rock.edu>
2016-06-23 01:22:29 -0400

    fixed formatting and indents

--- js/loadcontent.js
+++ js/loadcontent.js
@@ -1,9 +1,9 @@
-$(function(){
-  $("#header").load("include/header.html");
-  $("#footer").load("include/footer.html");
-  $("#homepage").load("include/homepage.html");
-  $("#portfolio").load("include/portfolio.html");
-  $("#resume").load("include/resume.html");
-  $("#contact").load("include/contact.html");
-  $("#about").load("include/about.html");
+$(function () {
+    $("#header").load("include/header.html");
+    $("#footer").load("include/footer.html");
+    $("#homepage").load("include/homepage.html");
+    $("#portfolio").load("include/portfolio.html");
+    $("#resume").load("include/resume.html");
+    $("#contact").load("include/contact.html");
+    $("#about").load("include/about.html");
 });
