a70617c2af7b32d0906c9e6ccb8575e5afe9d505
Vijay Pillai <vijay.pillai397@gmail.com>
2017-09-02 01:12:40 -0400

    started blog, branch wip

--- js/loadcontent.js
+++ js/loadcontent.js
@@ -1,11 +1,11 @@
 $(function() {
-    $("#header").load("include/header.html");
-    $("#footer").load("include/footer.html");
-    $("#homepage").load("include/homepage.html");
-    $("#portfolio").load("include/portfolio.html");
-    $("#resume").load("include/resume.html");
-    $("#resume_mobile").load("include/resume_mobile.html");
-    $("#contact").load("include/contact.html");
-    $("#about").load("include/about.html");
-    $("#skillset").load("include/skillset.html");
+    $("#header").load("/include/header.html");
+    $("#footer").load("/include/footer.html");
+    $("#homepage").load("/include/homepage.html");
+    $("#portfolio").load("/include/portfolio.html");
+    $("#resume").load("/include/resume.html");
+    $("#resume_mobile").load("/include/resume_mobile.html");
+    $("#contact").load("/include/contact.html");
+    $("#about").load("/include/about.html");
+    $("#skillset").load("/include/skillset.html");
 });
